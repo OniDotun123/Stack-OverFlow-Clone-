a0b545a5609db9086c7651679d755cfb1d989736
La-Keisha Towner  & Adedotun Oni  & Eric Prostko <pair+lm-towner.onidotun123.prostko@devbootcamp.com>
2017-02-23 17:10:17 -0500

    CRUD for questions controller complete

--- app/controllers/questions.rb
+++ app/controllers/questions.rb
@@ -3,7 +3,38 @@ get '/questions' do
   erb :"/questions/index"
 end
 
+get '/questions/new' do
+  erb :'/questions/new'
+end
+
+post '/questions' do
+  @question = Question.new(params[:question])
+  @question.update_attributes(user_id: session[:user_id]) #assigns user id to question.
+  if @question.save
+    redirect '/questions'
+  else
+    erb :'/questions/new'
+  end
+end
+
 get '/questions/:id' do
   @question = Question.find_by(id: params[:id])
   erb :'/questions/show'
+end
+
+get '/questions/:id/edit' do
+  @question = Question.find_by(id: params[:id])
+  erb :'/questions/edit'
+end
+
+put '/questions/:id' do
+  question = Question.find_by(id: params[:id])
+  question.update_attributes(params[:question])
+  redirect "/questions/#{params[:id]}"
+end
+
+delete '/questions/:id' do
+  @question = Question.find_by(id: params[:id])
+  @question.destroy
+  redirect '/questions'
 end
