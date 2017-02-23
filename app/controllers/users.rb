b9ab62e465f082c3be3a92a7c6983976d4a7f3c1
La-Keisha Towner  & Adedotun Oni  & Eric Prostko <pair+lm-towner.onidotun123.prostko@devbootcamp.com>
2017-02-23 15:30:11 -0500

    user login + user new and layout links

---
+++ app/controllers/users.rb
@@ -0,0 +1,28 @@
+get '/users/new' do
+
+  erb :'/users/new'
+end
+
+post '/users' do
+  @user = User.new(params[:user])
+    if @user.save
+      session[:user_id] = @user.id #logic that signs user in
+      redirect '/'
+    else
+      erb :'/users/new'
+    end
+end
+
+get '/users/login' do
+  erb :'/users/login'
+end
+
+post '/login' do
+    @user = User.find_by(email: params[:email])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      erb :'/users/show'
+    else
+      erb :'/users/login'
+    end
+end
