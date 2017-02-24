b6bf9d635b4d1f4ea9fbafc683134fd85359bfbb
La-Keisha Towner  & Adedotun Oni  & Eric Prostko <pair+lm-towner.onidotun123.prostko@devbootcamp.com>
2017-02-24 10:57:27 -0500

    Updated associations for question

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,7 @@ class Question < ActiveRecord::Base
   # Remember to create a migration!
   has_many :votes, as: :votable
   has_many :comments, as: :commentable
+  has_many :answers 
 
   validates :title, :content, presence: true
 end
