ca7f2402c65e627b6fd75b7a2b516f864b222a9e
La-Keisha Towner  & Adedotun Oni  & Eric Prostko <pair+lm-towner.onidotun123.prostko@devbootcamp.com>
2017-02-24 11:21:22 -0500

    Authorize user helper

--- app/helpers/authorize.rb
+++ app/helpers/authorize.rb
@@ -1,11 +1,18 @@
+#Returns the user object where the session id matches the user id.
 def current_user
     @current_user ||= User.find_by(id: session[:user_id])
 end
 
+#checks to see if the person is logged in at all.
 def require_login
   redirect '/users/login' unless session[:user_id]
 end
 
+#returns true or false if they're logged in.
 def login?
   !!current_user
+end
+
+def authorized?(class_name)
+  current_user == class_name.author
 end
